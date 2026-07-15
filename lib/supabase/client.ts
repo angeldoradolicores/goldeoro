a1a24b21d54eaf66152aaa5cf39128c7f45b06ee
Miguel Eraso <migueleraso@MacBook-Pro-de-Miguel.local>
2026-07-15 12:22:33 -0500

    correccion docker 4 final 2

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,11 +1,11 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
+  console.log("NEXT_PUBLIC_SUPABASE_URL =", process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY =", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL
   const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY
 
-  return createBrowserClient(
-    url!,
-    anonKey!
-  )
+  return createBrowserClient(url!, anonKey!)
 }
